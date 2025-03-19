940cae9fd3b24231d1f23c586611d69a2dfd52aa
shahzadali61 <noahdeveloper96@gmail.com>
2025-03-19 14:57:46 +0500

    add card in the dahboard

---
+++ resources/js/components/admin/DashboardCard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{
+    title: string;
+    value: number | string;
+    icon: any;
+    bgColor?: string;
+}>();
+</script>
+
+<template>
+    <a-card class="dashboard-card">
+        <div class="grid grid-cols-2">
+            <div>
+                <h3 class="text-xl text-gray-400">{{ title }}</h3>
+                <h4 class="text-2xl font-bold mb-0">{{ value }}</h4>
+            </div>
+            <div class="flex justify-end">
+                <div class="badge-icon flex justify-center items-center text-white" :class="bgColor">
+                    <component :is="icon" />
+                </div>
+            </div>
+        </div>
+    </a-card>
+</template>
+
+<style scoped>
+.badge-icon {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
--- resources/js/pages/admin/Dashboard.vue
+++ resources/js/pages/admin/Dashboard.vue
@@ -1,12 +1,25 @@
 <script setup lang="ts">
 import AdminLayout from '@/layouts/AdminLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { FileProtectOutlined } from '@ant-design/icons-vue';
+import DashboardCard from '@/components/admin/DashboardCard.vue';
 
 </script>
 
 <template>
-     <Head title="Brand" />
-  <AdminLayout>
-    <h1 >Dashboard</h1>
-    <p >Welcome to the admin panel!</p>
-  </AdminLayout>
+    <AdminLayout>
+        <Head title="Dashboard" />
+        <div style="background-color: #ececec; padding: 20px">
+            <a-row :gutter="10">
+                <a-col :span="6">
+                    <DashboardCard
+                        title="Brand"
+                        :value="33"
+                        :icon="FileProtectOutlined"
+                        bgColor="bg-yellow-800"
+                    />
+                </a-col>
+            </a-row>
+        </div>
+    </AdminLayout>
 </template>
